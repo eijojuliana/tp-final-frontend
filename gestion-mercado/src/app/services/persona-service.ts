c11acac003666e41c36296bb08d3e587a6d5e7c7
DloguPia <162518515+DloguPia@users.noreply.github.com>
2025-11-11 01:47:01 -0300

    metodos CRUD en persona-service
    se añadieron los metodos crud en persona-service

--- gestion-mercado/src/app/services/persona-service.ts
+++ gestion-mercado/src/app/services/persona-service.ts
@@ -1,6 +1,8 @@
+import { newPersona } from './../models/persona.model';
 import { Injectable, signal } from '@angular/core';
 import { Persona } from '../models/persona.model';
 import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,13 +14,16 @@ export class PersonaService {
   private personasState = signal<Persona[]>([]);
   public personas = this.personasState.asReadonly();
 
+  private personaToEditState=signal<Persona | null>(null);
+  public personaToEdit=this.personaToEditState.asReadonly();
+
 
   constructor(private http: HttpClient){
     this.load();
   }
 
 
-  private load() {
+   load() {
     this.http.get<Persona[]>(this.apiUrl).subscribe(
       data => {
         console.log(data)
@@ -27,5 +32,40 @@ export class PersonaService {
     );
   }
 
+  post(persona:newPersona) :Observable<Persona>{
+    return this.http.post<Persona>(this.apiUrl,persona).pipe(
+    tap(newPersona =>{
+       this.personasState.update(currentPersona => [...currentPersona,newPersona])
+      })
+    )
+  }
 
+  delete(id:number):Observable<Persona>{
+    return this.http.delete<Persona>(`${this.apiUrl}/${id}`).pipe(
+      tap(()=> {
+        this.personasState.update(currentPersona =>
+          currentPersona.filter(persona => persona.persona_id !== id )
+        )
+      })
+    )
+  }
+
+  update (personaToUpdate:Persona):Observable<Persona>{
+    return this.http.put<Persona>(`${this.apiUrl}/${personaToUpdate.persona_id}`,personaToUpdate).pipe(
+      tap(updatedPersona => {
+        this.personasState.update(currentPersona =>
+             currentPersona.map(p=>
+              p.persona_id=== updatedPersona.persona_id ? updatedPersona:p)
+        )
+      })
+    )
+  }
+
+  selectPersonaToEdit(persona:Persona){
+    this.personaToEditState.set(persona);
+  }
+
+  clearPersonaToEdit(){
+    this.personaToEditState.set(null);
+  }
 }
