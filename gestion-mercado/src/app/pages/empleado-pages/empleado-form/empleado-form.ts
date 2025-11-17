7b8fffebf28642f33c3dc165cd36ea843fe22f49
Gemerius <agusrozada2@gmail.com>
2025-11-16 22:31:04 -0300

    crud y filtro empleado

--- gestion-mercado/src/app/pages/empleado-pages/empleado-form/empleado-form.ts
+++ gestion-mercado/src/app/pages/empleado-pages/empleado-form/empleado-form.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, effect, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { EmpleadoService } from '../../../services/empleado-service';
+import { Router } from '@angular/router';
+import { Empleado, NewEmpleado } from '../../../models/empleado.model';
 
 @Component({
   selector: 'app-empleado-form',
@@ -9,4 +12,90 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class EmpleadoForm {
 
+  private fb=inject(FormBuilder);
+  empleadoService=inject(EmpleadoService);
+  private router=inject(Router);
+
+  isEditMode=signal(false);
+  private empleadoToEdit:Empleado|null=null;
+
+  form=this.fb.nonNullable.group({
+    nombre:['',[Validators.required,Validators.maxLength(20),Validators.pattern(/^[A-Za-zÁÉÍÓÚÜáéíóúüÑñ\s]+$/)]],
+    dni:[0,[Validators.required,Validators.pattern(/^\d{6,9}$/)]],
+    edad:[0,[Validators.required,Validators.min(18),Validators.max(120)]],
+    email:['',[Validators.required,Validators.email]]
+  })
+
+
+  constructor(){
+
+    effect(()=>{
+
+      this.empleadoToEdit=this.empleadoService.empleadoToEdit();
+
+      if(this.empleadoToEdit){
+        this.isEditMode.set(true);
+
+        this.form.patchValue({
+          nombre:this.empleadoToEdit.nombre,
+          dni:this.empleadoToEdit.dni,
+          edad:this.empleadoToEdit.edad,
+          email:this.empleadoToEdit.email
+        })
+      }else{
+        this.isEditMode.set(false);
+        this.form.reset();
+      }
+    })
+  }
+
+
+  saveEmpleado(){
+
+  if(this.form.invalid){return;}
+
+    const formValue:NewEmpleado=this.form.getRawValue();
+
+    if(this.isEditMode()&&this.empleadoToEdit){
+
+      const updateEmpleado:Empleado={...this.empleadoToEdit,...formValue};
+
+      this.empleadoService.update(updateEmpleado).subscribe(()=>{
+        console.log("Empleado eliminado");
+        this.empleadoService.clearEmpleadoToEdit();
+
+      }
+      )
+    }else{
+      this.empleadoService.agregar(formValue).subscribe(()=>{
+        this.form.reset();
+      })
+    }
+    this.router.navigate(['/menu/empleados']);
+  }
+
+
+  cancelEdit(){
+    this.empleadoService.clearEmpleadoToEdit();
+    this.router.navigate(['/menu/empleados']);
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //fin
 }
--- gestion-mercado/src/app/pages/empleado-pages/empleado-list/empleado-list.ts
+++ gestion-mercado/src/app/pages/empleado-pages/empleado-list/empleado-list.ts
@@ -1,6 +1,6 @@
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { EmpleadoService } from './../../../services/empleado-service';
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 
 @Component({
@@ -12,4 +12,39 @@ import { Component, inject } from '@angular/core';
 export class EmpleadoList {
  service=inject(EmpleadoService);
  empleados=this.service.empleados;
+ router=inject(Router);
+
+filtro = signal('');
+  atributo = signal<'nombre' | 'dni'|'edad'|'email'>('nombre');
+
+  empleadosFiltrados = computed(() => {
+    const f = this.filtro().toLowerCase().trim();
+    const attr = this.atributo();
+
+    return this.empleados().filter(e =>
+      String((e as any)[attr]).toLowerCase().includes(f)
+    );
+  });
+
+ eliminarEmpleado(id:number){
+  if(confirm("Desea eliminar este empleado?")){
+    this.service.eliminar(id).subscribe(()=>
+      console.log(`Empleado con id ${id} eliminado.`)
+    );
+  }
+ }
+
+ modificarEmpleado(empleado:any){
+  this.service.selectEmpleadoToEdit(empleado);
+  this.router.navigate(['menu/empleados/form'])
+ }
+
+
+
+
+
+
+
+
+
 }
