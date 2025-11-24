9207daab99651175f59453c54b59506d94ca60ca
F3LL5 <dylanolivera565@gmail.com>
2025-11-24 02:21:50 -0300

    diseño responsive menu y tabla personas

--- gestion-mercado/src/app/services/ip.ts
+++ gestion-mercado/src/app/services/ip.ts
@@ -1,7 +1,7 @@
 // Es mejor tener una variable global para la IP de la API Rest antes que multiples declaraciones de la misma en cada service.
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/api',
+  apiBaseUrl: 'http://192.168.100.153:8080/api',
   apiIp: 'localhost',
   apiPort: 8080,
 };
