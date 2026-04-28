583dd37e6b4e2a833a98d62f73093625264eca39
DloguPia <162518515+DloguPia@users.noreply.github.com>
2026-04-28 02:22:55 -0300

    Logo
    Cambiamos el logo,ahora es un gatito

--- gestion-mercado/src/app/pages/menu-page/menu-page.ts
+++ gestion-mercado/src/app/pages/menu-page/menu-page.ts
@@ -1,6 +1,8 @@
+import { Theme } from './../../styles/theme.service';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { ThemeService } from '../../styles/theme.service';
 
 @Component({
   selector: 'app-home-page',
@@ -11,10 +13,12 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class MenuPage {
  public authService=inject(AuthService);
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService , public theme: ThemeService) { }
 
   get currentRole(): string | null {
     return this.auth.getRole();
   }
+
+
 
 }
