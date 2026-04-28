3bf21e5f3e412db197619b11132b0d3015c2215b
DloguPia <162518515+DloguPia@users.noreply.github.com>
2026-04-28 01:59:17 -0300

    Boceto del menu
    un boceto del menu ademas de cambios , este no es el boceto final

--- gestion-mercado/src/app/pages/menu-page/menu-page.ts
+++ gestion-mercado/src/app/pages/menu-page/menu-page.ts
@@ -1,16 +1,16 @@
-import { RouterLink } from '@angular/router';
-import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './menu-page.html',
   styleUrl: './menu-page.css',
 })
 export class MenuPage {
-
+ public authService=inject(AuthService);
   constructor(public auth: AuthService) { }
 
   get currentRole(): string | null {
