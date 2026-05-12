20f3591134f5f7c4a4e16ac2b05ce043afc7c684
DloguPia <162518515+DloguPia@users.noreply.github.com>
2026-05-12 01:00:54 -0300

    Sidebar y login
    sacamos el sidebar del login babyssssssssss

--- gestion-mercado/src/app/app.ts
+++ gestion-mercado/src/app/app.ts
@@ -1,17 +1,32 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router'; 
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
 import { ToastComponent } from "./components/toast-component/toast.component";
 import { BreadcrumbComponent } from './components/breadcrumb.component/breadcrumb.component';
 import { Sidebar } from './components/sidebar/sidebar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, BreadcrumbComponent,Sidebar],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    Header,
+    Footer,
+    BreadcrumbComponent,
+    Sidebar,
+    CommonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('gestion-mercado');
+
+  constructor(private router: Router) {}
+
+  isLoginRoute(): boolean {
+    return this.router.url === '/login';
+  }
 }
