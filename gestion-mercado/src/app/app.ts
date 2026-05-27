c0158699be3c5a0b8a9dfa04a1609210431666cd
F3LL5 <dylanolivera565@gmail.com>
2026-05-27 15:42:40 -0300

    Logo como marca de agua y productos con icono sakura

--- gestion-mercado/src/app/app.ts
+++ gestion-mercado/src/app/app.ts
@@ -6,6 +6,7 @@ import { ToastComponent } from "./components/toast-component/toast.component";
 import { BreadcrumbComponent } from './components/breadcrumb.component/breadcrumb.component';
 import { Sidebar } from './components/sidebar/sidebar';
 import { CommonModule } from '@angular/common';
+import { ThemeService } from './styles/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -23,11 +24,23 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.css'
 })
 export class App {
+  animating = false;
+
   protected readonly title = signal('gestion-mercado');
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, public theme: ThemeService) {}
 
   isLoginRoute(): boolean {
     return this.router.url === '/login';
+  }
+
+  ngOnInit(): void {
+    this.theme.init();
+  }
+
+  toggleTheme(): void {
+    this.animating = true;
+    this.theme.toggle();
+    setTimeout(() => (this.animating = false), 200);
   }
 }
